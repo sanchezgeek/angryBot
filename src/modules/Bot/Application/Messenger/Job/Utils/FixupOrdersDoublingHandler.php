9bdb27f5c1fe827c6ba0adeed32b39d2c23886fb
Sanchez <sanchezgeek@gmail.com>
2023-03-11 22:01:33 +0300

    FixupOrdersDoublingHandler

--- src/modules/Bot/Application/Messenger/Job/Utils/FixupOrdersDoublingHandler.php
+++ src/modules/Bot/Application/Messenger/Job/Utils/FixupOrdersDoublingHandler.php
@@ -9,7 +9,6 @@ use App\Bot\Domain\Entity\BuyOrder;
 use App\Bot\Domain\Entity\Stop;
 use App\Bot\Domain\Repository\StopRepository;
 use App\Bot\Domain\ValueObject\Order\OrderType;
-use App\Bot\Domain\ValueObject\Position\Side;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Query\Expr\OrderBy;
 use Doctrine\ORM\QueryBuilder;
@@ -31,21 +30,6 @@ final class FixupOrdersDoublingHandler
             ? $this->buyOrderRepository
             : $this->stopRepository;
 
-        // @todo разобраться почему SHORT SL не группируются и не вычищаются
-//        if ($message->orderType === OrderType::Stop && $message->positionSide === Side::Sell) {
-//            $orders = $repository->findActive(
-//                side: $message->positionSide,
-//                exceptOppositeOrders: true,
-//                qbModifier: static fn (QueryBuilder $qb) => $qb->andWhere($qb->getRootAliases()[0] . '.price < 22250.5')->addOrderBy($qb->getRootAliases()[0] . '.price', 'desc')
-//            );
-//        } else {
-//            $orders = $repository->findActive(
-//                side: $message->positionSide,
-//                exceptOppositeOrders: true,
-//                qbModifier: static fn (QueryBuilder $qb) => $qb->addOrderBy(new OrderBy($qb->getRootAliases()[0] . '.price', 'desc'))
-//            );
-//        }
-
         $orders = $repository->findActive(
             side: $message->positionSide,
 //            exceptOppositeOrders: true,
@@ -62,11 +46,12 @@ final class FixupOrdersDoublingHandler
         $stepOrders = [];
         $stepBottom = ceil($orders[0]->getPrice()) - $message->step;
         while ($order = \array_shift($orders)) {
-            if ($order->getPrice() > $stepBottom) {
-                $stepOrders[] = $order;
-            } else {
+            /** @var Stop|BuyOrder $order */
+            if ($order->getPrice() < $stepBottom || !count($orders)) {
                 $stepBottom = $stepBottom - $message->step;
-                \array_unshift($orders, $order); // Push back
+                if (\count($orders)) {
+                    \array_unshift($orders, $order); // Push back in case of further iteration
+                }
                 \usort($stepOrders, static fn(Stop|BuyOrder $a, Stop|BuyOrder $b) => $a->getVolume() <=> $b->getVolume());
 
                 $removedVolume = 0;
@@ -74,25 +59,19 @@ final class FixupOrdersDoublingHandler
                     $forRemove[] = ($removed = \array_shift($stepOrders));
                     $removedVolume += $removed->getVolume();
                 }
-//                if ($message->orderType === OrderType::Stop && $message->positionSide === Side::Sell) {
-//                    var_dump(
-//                        $stepBottom,
-//                        $stepOrders,
-//                        $removedVolume
-//                    );die;
-//                }
 
                 // Add volume to first in left group
                 if ($removedVolume && $message->groupInOne) {
                     $firstInGroup = \array_shift($stepOrders);
+                    $firstInGroup->addVolume($removedVolume);
 
-                    $this->entityManager->persist(
-                        $firstInGroup->addVolume($removedVolume)
-                    );
+                    $this->entityManager->persist($firstInGroup);
                     $this->entityManager->flush();
                 }
 
                 $stepOrders = [];
+            } else {
+                $stepOrders[] = $order;
             }
         }
 
