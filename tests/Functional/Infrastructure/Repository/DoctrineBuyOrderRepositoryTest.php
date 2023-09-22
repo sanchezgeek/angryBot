e98bc30e6d86906d81582be1ddcca8a0ad6be300
Sanchez <sanchezgeek@gmail.com>
2023-09-23 01:13:44 +0300

    #tests | BuyOrderRepository test

--- tests/Functional/Infrastructure/Repository/DoctrineBuyOrderRepositoryTest.php
+++ tests/Functional/Infrastructure/Repository/DoctrineBuyOrderRepositoryTest.php
@@ -40,8 +40,12 @@ final class DoctrineBuyOrderRepositoryTest extends KernelTestCase
     {
         // Arrange
         $buyOrder = new BuyOrder(1, 100500, 123.456, 10, $side, ['someStringContext' => 'some value', 'someArrayContext' => ['value']]);
-        $exchangeOrderId = uuid_create(); $buyOrder->setExchangeOrderId($exchangeOrderId);
-        $otherExchangeOrderId = uuid_create(); $buyOrder->setOnlyAfterExchangeOrderExecutedContext($otherExchangeOrderId);
+        $buyOrder->setExchangeOrderId(
+            $buyOrderExchangeOrderId = uuid_create()
+        );
+        $buyOrder->setOnlyAfterExchangeOrderExecutedContext(
+            $stopExchangeOrderId = uuid_create()
+        );
 
         // Act
         $this->buyOrderRepository->save($buyOrder);
@@ -49,8 +53,30 @@ final class DoctrineBuyOrderRepositoryTest extends KernelTestCase
         // Assert
         self::seeBuyOrdersInDb(
             (new BuyOrder(1, 100500, 123.456, 10, $side, ['someStringContext' => 'some value', 'someArrayContext' => ['value']]))
-                ->setExchangeOrderId($exchangeOrderId)
-                ->setOnlyAfterExchangeOrderExecutedContext($otherExchangeOrderId)
+                ->setExchangeOrderId($buyOrderExchangeOrderId)
+                ->setOnlyAfterExchangeOrderExecutedContext($stopExchangeOrderId)
+        );
+    }
+
+    /**
+     * @dataProvider positionSideProvider
+     */
+    public function testRemove(Side $side): void
+    {
+        // Arrange
+        $this->applyDbFixtures(
+            new BuyOrderFixture(new BuyOrder(1, 1050, 123.123, 10, $side)),
+            new BuyOrderFixture(new BuyOrder(2, 1050, 123.123, 10, $side)),
+        );
+
+        $buyOrder = $this->buyOrderRepository->find(2);
+
+        // Act
+        $this->buyOrderRepository->remove($buyOrder);
+
+        // Assert
+        self::seeBuyOrdersInDb(
+            new BuyOrder(1, 1050, 123.123, 10, $side)
         );
     }
 
@@ -82,11 +108,29 @@ final class DoctrineBuyOrderRepositoryTest extends KernelTestCase
             new BuyOrderFixture(new BuyOrder(1, 100500, 123.123, 10, $side)),
             new BuyOrderFixture((new BuyOrder(100, 2050, 223.1, 10, $side))->setExchangeOrderId('123456')),
             new BuyOrderFixture((new BuyOrder(101, 2050, 223.1, 10, $side, ['someContext' => 'some value', 'someArrayContext' => ['value']]))),
-            new BuyOrderFixture((new BuyOrder(1000, 3050, 323, 10, $side))),
+            new BuyOrderFixture(new BuyOrder(1000, 3050, 323, 10, $side)),
         );
 
         self::assertEquals([
             new BuyOrder(101, 2050, 223.1, 10, $side, ['someContext' => 'some value', 'someArrayContext' => ['value']]),
         ], $this->buyOrderRepository->findActiveInRange($side, 2000, 3000));
+    }
+
+    /**
+     * @dataProvider positionSideProvider
+     */
+    public function testCanFindOppositeToStopByExchangeOrderId(Side $side): void
+    {
+        $exchangeOrderId = uuid_create();
+
+        $this->applyDbFixtures(
+            new BuyOrderFixture(new BuyOrder(1, 100500, 123.123, 10, $side)),
+            new BuyOrderFixture((new BuyOrder(100, 2050, 223.1, 10, $side))->setOnlyAfterExchangeOrderExecutedContext($exchangeOrderId)),
+            new BuyOrderFixture(new BuyOrder(1000, 3050, 323, 10, $side)),
+        );
+
+        self::assertEquals([
+            (new BuyOrder(100, 2050, 223.1, 10, $side))->setOnlyAfterExchangeOrderExecutedContext($exchangeOrderId),
+        ], $this->buyOrderRepository->findOppositeToStopByExchangeOrderId($side, $exchangeOrderId));
     }
 }
