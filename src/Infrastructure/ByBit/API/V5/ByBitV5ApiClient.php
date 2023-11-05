0e9abe5acb4bdfdb640bd44251f36f9a510003fb
Sanchez <sanchezgeek@gmail.com>
2023-11-06 01:22:23 +0300

    #apiV5 | RECV_WINDOW

--- src/Infrastructure/ByBit/API/V5/ByBitV5ApiClient.php
+++ src/Infrastructure/ByBit/API/V5/ByBitV5ApiClient.php
@@ -40,7 +40,7 @@ use function sprintf;
  */
 final readonly class ByBitV5ApiClient implements ByBitApiClientInterface
 {
-    private const BAPI_RECOMMENDED_RECV_WINDOW = '5000';
+    private const BAPI_RECOMMENDED_RECV_WINDOW = '8000';
     private const BAPI_SIGN_TYPE = '2';
 
     public function __construct(
