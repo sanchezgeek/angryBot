525e9be4f7ef856d4bacfee6eae4c20cb798f0fb
Sanchez <sanchezgeek@gmail.com>
2023-12-31 23:57:41 +0300

    #utils | funding fees transfer: add retry | remove logs

--- src/Application/Messenger/Market/TransferFundingFeesHandler.php
+++ src/Application/Messenger/Market/TransferFundingFeesHandler.php
@@ -7,16 +7,16 @@ namespace App\Application\Messenger\Market;
 use App\Bot\Application\Service\Exchange\Account\ExchangeAccountServiceInterface;
 use App\Bot\Application\Service\Exchange\MarketServiceInterface;
 use App\Bot\Application\Service\Exchange\PositionServiceInterface;
-use App\Clock\ClockInterface;
 use App\Domain\Price\Helper\PriceHelper;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Throwable;
+
+use function sleep;
 
 #[AsMessageHandler]
 final readonly class TransferFundingFeesHandler
 {
     public function __construct(
-        private ClockInterface $clock,
         private MarketServiceInterface $marketService,
         private PositionServiceInterface $positionService,
         private ExchangeAccountServiceInterface $exchangeAccountService,
@@ -25,28 +25,37 @@ final readonly class TransferFundingFeesHandler
 
     public function __invoke(TransferFundingFees $message): void
     {
-//        var_dump(sprintf('imhere. dispatched: %s, now: %s', $message->getDispatchedDateTime()->format('H:i:s'), $this->clock->now()->format('H:i:s')));
         $symbol = $message->symbol;
         $positionSide = $message->side;
 
         $position = $this->positionService->getPosition($symbol, $positionSide);
 
+        $prevPeriodRate = PriceHelper::round($this->marketService->getPreviousPeriodFundingRate($symbol), 7);
+        var_dump(sprintf('prev period funding rate: %.7f', $prevPeriodRate));
+
         if (!$position) {
             return;
         }
 
-        $prevPeriodRate = PriceHelper::round($this->marketService->getPreviousPeriodFundingRate($symbol), 7);
-        var_dump(sprintf('prev period funding rate: %.7f', $prevPeriodRate));
-
-        $fee = PriceHelper::round($position->value * $prevPeriodRate, 4);
+        $sign = $position->isShort() ? 1 : -1;
+        $fee = $sign * PriceHelper::round($position->value * $prevPeriodRate, 4);
         $coin = $symbol->associatedCoin();
-        $direction = $fee > 0 ? 'from contract to spot' : 'from spot to contract';
 
-        if ($fee > 0) {
-            $this->exchangeAccountService->interTransferFromContractToSpot($coin, $fee);
-        } else {
-            $this->exchangeAccountService->interTransferFromSpotToContract($coin, $fee);
+        $tries = 0;
+        $result = false;
+        while (!$result && $tries < 5) {
+            $tries++;
+            try {
+                if ($fee > 0) {
+                    $this->exchangeAccountService->interTransferFromContractToSpot($coin, $fee);
+                } elseif ($fee < 0) {
+                    $this->exchangeAccountService->interTransferFromSpotToContract($coin, -$fee);
+                }
+                $result = true;
+            } catch (Throwable $e) {
+                var_dump($e->getMessage());
+                sleep(1);
+            }
         }
-//        var_dump(sprintf('%.4f %s transferred %s (prev period funding rate: %.7f)', $fee, $coin->name, $direction, $prevPeriodRate));
     }
 }
