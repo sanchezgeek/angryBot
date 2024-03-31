d1053c6bf2eeeca1e5d9ef5f56c3ba97b4798c40
Sanchez <sanchezgeek@gmail.com>
2024-03-31 13:11:32 +0300

    #td: fix test

--- tests/Functional/Infrastructure/BybBit/Service/ByBitExchangeAccountService/ByBitExchangeAccountServiceTestAbstract.php
+++ tests/Functional/Infrastructure/BybBit/Service/ByBitExchangeAccountService/ByBitExchangeAccountServiceTestAbstract.php
@@ -9,6 +9,7 @@ use App\Infrastructure\ByBit\API\Common\Result\ApiErrorInterface;
 use App\Infrastructure\ByBit\Service\Account\ByBitExchangeAccountService;
 use App\Infrastructure\ByBit\Service\ByBitLinearExchangeService;
 use App\Tests\Mixin\Tester\ByBitV5ApiTester;
+use Psr\Log\LoggerInterface;
 use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Throwable;
@@ -23,11 +24,15 @@ abstract class ByBitExchangeAccountServiceTestAbstract extends KernelTestCase
     protected const WORKER_DEBUG_HASH = '123456';
 
     protected ByBitExchangeAccountService $service;
+    protected LoggerInterface $appErrorLogger;
 
     protected function setUp(): void
     {
+        $this->appErrorLogger = $this->createMock(LoggerInterface::class);
+
         $this->service = new ByBitExchangeAccountService(
-            $this->initializeApiClient()
+            $this->initializeApiClient(),
+            $this->appErrorLogger
         );
     }
 }
