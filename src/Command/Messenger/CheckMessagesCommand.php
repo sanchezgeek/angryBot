484f0f66ddfd35a978fe64f9d39c27371d953fb0
Sanchez <sanchezgeek@gmail.com>
2024-08-12 14:30:46 -0700

    #command / `messenger:check`

--- src/Command/Messenger/CheckMessagesCommand.php
+++ src/Command/Messenger/CheckMessagesCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 use function sprintf;
 
@@ -15,11 +16,18 @@ class CheckMessagesCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
         $connection = $this->entityManager->getConnection();
 
         $result = $connection->executeQuery('SELECT count(*) from messenger_messages')->fetch();
 
-        $output->writeln(sprintf('count: %d', $result['count']));
+        $count = (int) $result['count'];
+        $output->writeln(sprintf('count: %d', $count));
+
+        if ($count && $io->ask('remove?')) {
+            $connection->executeQuery('DELETE FROM messenger_messages WHERE 1=1')->fetch();
+        }
 
         return Command::SUCCESS;
     }
