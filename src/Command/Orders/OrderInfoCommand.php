eac03c44b15144cb5f4b406e71e5264f7388f4a1
Sanchez <sanchezgeek@gmail.com>
2024-10-25 05:00:11 -0700

    #orders / `o:info`: batch

--- src/Command/Orders/OrderInfoCommand.php
+++ src/Command/Orders/OrderInfoCommand.php
@@ -4,6 +4,7 @@ namespace App\Command\Orders;
 
 use App\Bot\Domain\Repository\BuyOrderRepository;
 use App\Bot\Domain\Repository\StopRepository;
+use App\Bot\Domain\ValueObject\Order\OrderType;
 use App\Command\AbstractCommand;
 use App\Helper\OutputHelper;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -16,17 +17,15 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'o:info')]
 class OrderInfoCommand extends AbstractCommand
 {
-    private const ID_ARG = 'id';
+    private const IDS_ARG = 'ids';
     private const REMOVE_OPTION = 'remove';
     private const EDIT_OPTION = 'edit';
-
-    /** `edit`-action options */
     public const EDIT_CALLBACK_OPTION = 'eC';
 
     protected function configure(): void
     {
         $this
-            ->addArgument(self::ID_ARG, InputArgument::REQUIRED, 'Order ID (prefixed with `b.` [buy] or `s.` [sell])')
+            ->addArgument(self::IDS_ARG, InputArgument::REQUIRED, 'Orders IDs (prefixed with `b.` [buy] or `s.` [sell]) separated by comma')
             ->addOption(self::REMOVE_OPTION, 'r', InputOption::VALUE_NEGATABLE, 'Remove?')
             ->addOption(self::EDIT_OPTION, null, InputOption::VALUE_NEGATABLE, 'Edit?')
             ->addOption(self::EDIT_CALLBACK_OPTION, null, InputOption::VALUE_REQUIRED, 'Edit callback')
@@ -36,40 +35,47 @@ class OrderInfoCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 //        while ($c = fread(STDIN, 16)) {$c = preg_replace('/[^[:print:]\n]/u', '', mb_convert_encoding($c, 'UTF-8', 'UTF-8')); if ($c === "[A") echo "UP"; if ($c === "[B") echo "DOWN"; if ($c === "[C")echo "RIGHT"; if ($c === "[D") echo "LEFT";}
-        $rawId = $this->paramFetcher->getStringArgument(self::ID_ARG);
-        if (!preg_match('/^([bs])\.\d+$/', $rawId)) {
-            throw new \InvalidArgumentException('Invalid order definition provided (good example: b.123 or s.123)');
-        }
+        $rawIds = $this->paramFetcher->getStringArgument(self::IDS_ARG);
+        $rawIds = explode(',', $rawIds);
 
-        $rawId = explode('.', $rawId);
-        $repository = $rawId[0] === 'b' ? $this->buyOrderRepository : $this->stopRepository;
-        $id = $rawId[1];
+        $orderType = null;
+        foreach ($rawIds as $rawId) {
+            assert(preg_match('/^([bs])\.\d+$/', $rawId), sprintf('Invalid order definition ("%s") provided (good example: b.123 or s.123)', $rawId));
+            $rawId = explode('.', $rawId);
 
-        if (!$order = $repository->find($id)) {
-            throw new \Exception('Order not found');
+            assert(!$orderType || $rawId[0] === $orderType, 'All provided orders must be of one type (buy / stop)');
+            $orderType = $rawId[0];
         }
+        $repository = $orderType === 'b' ? $this->buyOrderRepository : $this->stopRepository;
+        $orderType = $orderType === 'b' ? OrderType::Add : OrderType::Stop;
 
-        if ($this->paramFetcher->getBoolOption(self::REMOVE_OPTION)) {
-            $action = 'r';
-        } elseif ($this->paramFetcher->getBoolOption(self::EDIT_OPTION)) {
-            $action = 'e';
-        } else {
-            $action = $this->io->ask('Select action: "e" - edit, "r" - remove');
-        }
+        $action = match (true) {
+            $this->paramFetcher->getBoolOption(self::REMOVE_OPTION) => 'r',
+            $this->paramFetcher->getBoolOption(self::EDIT_OPTION) => 'e',
+            default => $this->io->ask('Select action: "e" - edit, "r" - remove'),
+        };
 
         $showInfo = true;
-        if ($action === 'e' && ($editCallback = $this->paramFetcher->getStringOption(self::EDIT_CALLBACK_OPTION, false))) {
+        if ($action === 'e' && ($globalEC = $this->paramFetcher->getStringOption(self::EDIT_CALLBACK_OPTION, false))) {
             $showInfo = false;
         }
-        $showInfo && OutputHelper::print($order->info());
 
-        if ($action === 'e') {
-            $editCallback = $editCallback ?? $this->io->ask('Set callback. E.g. `setPrice(100500)`, `setVolume(0.005), ...`');
-            eval('$order->' . $editCallback . ';');
-            $repository->save($order);
-        } elseif ($action === 'r') {
-            if ($this->io->confirm('Are you sure you want to remove this order?', false)) {
-                $repository->remove($order);
+        foreach ($rawIds as $rawId) {
+            $editCallback = null;
+            $id = explode('.', $rawId);
+            $order = $repository->find($id[1]);
+            assert($order !== null, sprintf('Cannot find %s order with id=%d', $orderType->title(), $id[1]));
+
+            $showInfo && OutputHelper::print($order->info());
+
+            if ($action === 'e') {
+                $editCallback = $globalEC ?? $this->io->ask(sprintf('Set callback for "%s". E.g. `setPrice(100500)`, `setVolume(0.005), ...`', $rawId));
+                eval('$order->' . $editCallback . ';');
+                $repository->save($order);
+            } elseif ($action === 'r') {
+                if ($this->io->confirm('Are you sure you want to remove this order?', false)) {
+                    $repository->remove($order);
+                }
             }
         }
 
