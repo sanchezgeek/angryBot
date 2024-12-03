314e6b805a569b4f699bde4e7f44caf42445dafc
Sanchez <sanchezgeek@gmail.com>
2024-12-03 21:02:39 +0300

    LogReceivedMessageTimestampDebugListener

--- src/Application/Messenger/Position/CheckPositionIsUnderLiquidation.php
+++ src/Application/Messenger/Position/CheckPositionIsUnderLiquidation.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace App\Application\Messenger\Position;
 
 use App\Bot\Domain\ValueObject\Symbol;
-use App\Infrastructure\Symfony\Messenger\Async\Debug\MessageWithDispatchingTimeTrait;
 
 /**
  * @codeCoverageIgnore
  */
-final class CheckPositionIsUnderLiquidation
+final readonly class CheckPositionIsUnderLiquidation
 {
-    use MessageWithDispatchingTimeTrait;
-
-    public function __construct(public readonly Symbol $symbol) {}
+    public function __construct(public Symbol $symbol) {}
 }
