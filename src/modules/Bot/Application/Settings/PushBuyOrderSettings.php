1293304813e1087e828376cf6ef6ef7fe2aaad77
Sanchez <sanchezgeek@gmail.com>
2024-12-06 17:59:17 +0300

    #settings: checks while pushing orders

---
+++ src/modules/Bot/Application/Settings/PushBuyOrderSettings.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Bot\Application\Settings;
+
+use App\Settings\Application\Service\SettingKeyAware;
+
+enum PushBuyOrderSettings: string implements SettingKeyAware
+{
+    case Checks_lastPriceOverIndexPriceCheckEnabled = 'push.BuyOrder.checks.isLastPriceOverIndexPrice.enabled';
+    case Checks_ignoreBuyBasedOnTotalPositionLeverageEnabled = 'push.BuyOrder.checks.ignoreBuyBasedOnTotalPositionLeverage.enabled';
+
+    public function getSettingKey(): string
+    {
+        return $this->value;
+    }
+}
