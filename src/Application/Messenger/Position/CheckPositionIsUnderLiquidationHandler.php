0111591fc3f3b630cdc3726e260462d67504a5fd
Sanchez <sanchezgeek@gmail.com>
2024-12-16 02:32:36 +0300

    liquidation: settings

--- src/Application/Messenger/Position/CheckPositionIsUnderLiquidationHandler.php
+++ src/Application/Messenger/Position/CheckPositionIsUnderLiquidationHandler.php
@@ -50,13 +50,13 @@ final class CheckPositionIsUnderLiquidationHandler
     public const TRANSFER_AMOUNT_MODIFIER = 0.2;
 
     # Additional stop
-    public const PERCENT_OF_LIQUIDATION_DISTANCE_TO_ADD_STOP_BEFORE = 40;
+    public const PERCENT_OF_LIQUIDATION_DISTANCE_TO_ADD_STOP_BEFORE = 30;
 
     # To check stopped position volume
     public const ACTUAL_STOPS_RANGE_FROM_ADDITIONAL_STOP = 7;
     public const CLOSE_BY_MARKET_IF_DISTANCE_LESS_THAN = 20;
 
-    public const ACCEPTABLE_STOPPED_PART = 15;
+    public const ACCEPTABLE_STOPPED_PART = 8;
     private const ACCEPTABLE_STOPPED_PART_MODIFIER = 0.2;
 
     const SPOT_TRANSFERS_BEFORE_ADD_STOP = 2.5;
