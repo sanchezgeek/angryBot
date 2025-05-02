7f389297ba0e886a3da990f469e060351b44fc45
Sanchez <sanchezgeek@gmail.com>
2025-05-02 02:17:41 -0700

    #service: reload config if changed

--- src/modules/Service/Infrastructure/Job/GenerateSupervisorConfigs/GenerateSupervisorConfigsHandler.php
+++ src/modules/Service/Infrastructure/Job/GenerateSupervisorConfigs/GenerateSupervisorConfigsHandler.php
@@ -15,17 +15,22 @@ final readonly class GenerateSupervisorConfigsHandler
     {
         $finder = new Finder();
 
+        $prevContent = [];
         $finder->files()->in($this->configsPath);
         if ($finder->hasResults()) {
             foreach ($finder as $file) {
                 if (!str_contains($file->getFilename(), self::CONFIG_FILE_NAME_PREFIX)) {
                     continue;
                 }
+                $prevContent[$file->getFilename()] = file_get_contents($file->getRealPath());
 
                 unlink($file->getRealPath());
             }
         }
+        sort($prevContent);
+        $prevContent = json_encode($prevContent);
 
+        $newContent = [];
         $templatePath = realpath($this->templatePath);
         $templateContent = file_get_contents($templatePath);
         foreach ($this->positionService->getOpenedPositionsSymbols() as $symbol) {
@@ -35,6 +40,14 @@ final readonly class GenerateSupervisorConfigsHandler
             $filename = sprintf('%s%s.ini', self::CONFIG_FILE_NAME_PREFIX, $symbol);
 
             file_put_contents($this->configsPath . DIRECTORY_SEPARATOR . $filename, $content);
+
+            $newContent[$filename] = $content;
+        }
+        sort($newContent);
+        $newContent = json_encode($newContent);
+
+        if ($prevContent !== $newContent) {
+            shell_exec('/usr/bin/supervisorctl reload');
         }
     }
 
