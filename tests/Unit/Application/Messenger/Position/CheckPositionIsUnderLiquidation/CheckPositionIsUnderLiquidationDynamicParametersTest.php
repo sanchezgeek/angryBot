8343a2da2995035639ade7741fffa16648d4fec0
Sanchez <sanchezgeek@gmail.com>
2025-06-06 00:29:19 -0700

    OpenPositionHandler: fix create stops

--- tests/Unit/Application/Messenger/Position/CheckPositionIsUnderLiquidation/CheckPositionIsUnderLiquidationDynamicParametersTest.php
+++ tests/Unit/Application/Messenger/Position/CheckPositionIsUnderLiquidation/CheckPositionIsUnderLiquidationDynamicParametersTest.php
@@ -198,6 +198,17 @@ final class CheckPositionIsUnderLiquidationDynamicParametersTest extends KernelT
             $message, $long, $ticker, $expectedPriceRange, $expectedStopPrice, $warningDistance, $criticalDistance, $acceptableStoppedPart,
         ];
 
+        // @todo | liquidation | corner cases
+//        $ticker = TickerFactory::withEqualPrices($symbol, 30001);
+//        $expectedPriceRange = PriceRange::create(30093.97, 30179.65);
+//        $expectedStopPrice = $symbol->makePrice(30099.0);
+//        $warningDistance = 301.0;
+//        $criticalDistance = 180.6;
+//        $acceptableStoppedPart = 66.4451827242525;
+//        yield self::wholeDataCaseDescription($message, $long, $ticker, $expectedPriceRange, $expectedStopPrice, $warningDistance, $criticalDistance, $acceptableStoppedPart, 'manual | corner.cases | liq. right after entry / ticker between entry and liquidation (in loss)') => [
+//            $message, $long, $ticker, $expectedPriceRange, $expectedStopPrice, $warningDistance, $criticalDistance, $acceptableStoppedPart, true
+//        ];
+//return;
         #### simple
         $long = PositionBuilder::long()->entry(30000)->size(1)->liq(25000)->build();
 
