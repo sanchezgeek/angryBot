3aac30d116e0671360e0b292d3c96b66030e99ef
Sanchez <sanchezgeek@gmail.com>
2025-07-05 16:49:37 +0300

    account: history

--- src/Command/Account/HistoryCommand.php
+++ src/Command/Account/HistoryCommand.php
@@ -4,11 +4,13 @@ namespace App\Command\Account;
 
 use App\Command\AbstractCommand;
 use App\Command\Mixin\ConsoleInputAwareCommand;
+use App\Helper\OutputHelper;
 use App\Infrastructure\ByBit\API\Common\ByBitApiClientInterface;
 use App\Infrastructure\ByBit\API\V5\Request\Asset\History\GetHistoryRequest;
 use App\Infrastructure\ByBit\Service\Common\ByBitApiCallHandler;
 use DateTimeImmutable;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -29,18 +31,23 @@ class HistoryCommand extends AbstractCommand
         $byBitApiCallResult = $this->sendRequest($request);
         $data = $byBitApiCallResult->data();
 
+        $list = array_filter($data['list'], static fn(array $item) => $item['type'] === 'SETTLEMENT');
         $data = array_map(static fn(
             array $item
         ) => [
-            'cashBalance' => $item['cashBalance'],
+            'cashBalance' => (float)$item['cashBalance'],
             'type' => $item['type'],
             'symbol' => $item['symbol'],
-            'funding' => $item['funding'],
-            'change' => $item['change'],
+            'funding' => (float)$item['funding'],
+            'change' => (float)$item['change'],
             'datetime' => new DateTimeImmutable()->setTimestamp($item['transactionTime'] / 1000)->format('d-m-Y H:i:s'),
-        ], $data['list']);
+        ], $list);
 
-        var_dump($data);die;
+        foreach ($data as $item) {
+            OutputHelper::print(sprintf('[%10s] %20s: %s %s %s => %s', $item['datetime'], $item['symbol'], $item['cashBalance'], $item['change'] > 0 ? '+' : '-', abs($item['change']), $item['cashBalance'] - $item['change']));
+        }
+
+        return Command::SUCCESS;
     }
 
     public function __construct(
