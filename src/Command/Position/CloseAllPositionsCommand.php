e6da5c7bf8b1003106a002ae76048efa9d06781f
Sanchez <sanchezgeek@gmail.com>
2025-07-12 12:01:51 +0300

    position / all: filter

--- src/Command/Position/CloseAllPositionsCommand.php
+++ src/Command/Position/CloseAllPositionsCommand.php
@@ -7,6 +7,7 @@ use App\Bot\Domain\Position;
 use App\Command\AbstractCommand;
 use App\Command\Mixin\ConsoleInputAwareCommand;
 use App\Command\Mixin\SymbolAwareCommand;
+use App\Domain\Value\Percent\Percent;
 use App\Infrastructure\ByBit\Service\ByBitLinearPositionService;
 use App\Trading\Application\Symbol\SymbolProvider;
 use App\Trading\Domain\Symbol\Helper\SymbolHelper;
@@ -22,7 +23,11 @@ class CloseAllPositionsCommand extends AbstractCommand
     use ConsoleInputAwareCommand;
     use SymbolAwareCommand;
 
+    public const string DEFAULT_PERCENT = '10';
+
     public const string FILTER_CALLBACKS_OPTION = 'fC';
+    public const string PERCENT_OPTION = 'percent';
+    public const string START_FROM_PNL_PERCENT = 'pnl-greater-than';
 
     private array $filterCallbacks = [];
 
@@ -31,6 +36,8 @@ class CloseAllPositionsCommand extends AbstractCommand
         $this
             ->configureSymbolArgs(defaultValue: null)
             ->addOption(self::FILTER_CALLBACKS_OPTION, null, InputOption::VALUE_REQUIRED, 'Filter callbacks')
+            ->addOption(self::PERCENT_OPTION, null, InputOption::VALUE_REQUIRED, 'Percent', self::DEFAULT_PERCENT)
+            ->addOption(self::START_FROM_PNL_PERCENT, null, InputOption::VALUE_REQUIRED, 'If pnl percent greater than ...')
         ;
     }
 
@@ -45,16 +52,31 @@ class CloseAllPositionsCommand extends AbstractCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $positionPart = $this->paramFetcher->requiredFloatOption(self::PERCENT_OPTION);
+        $positionPart = new Percent($positionPart);
+        $ifPnlGreaterThan = $this->paramFetcher->floatOption(self::START_FROM_PNL_PERCENT);
+
         $symbolsRaw = SymbolHelper::symbolsToRawValues(...$this->getSymbols());
-        var_dump($symbolsRaw);die;
 
         $res = [];
-        foreach ($this->positionService->getAllPositions() as $symbolRaw => $positions) {
+        $allPositions = $this->positionService->getAllPositions();
+        $lastPrices = $this->positionService->getLastMarkPrices();
+
+        foreach ($allPositions as $symbolRaw => $positions) {
             if (!in_array($symbolRaw, $symbolsRaw, true)) {
                 continue;
             }
 
+            $markPrice = $lastPrices[$symbolRaw];
+
             foreach ($positions as $position) {
+                if ($ifPnlGreaterThan) {
+                    $pnlPercent = $markPrice->getPnlPercentFor($position);
+                    if ($pnlPercent < $ifPnlGreaterThan) {
+                        continue;
+                    }
+                }
+
                 if (!$this->applyFilters($position)) {
                     continue;
                 }
@@ -64,7 +86,7 @@ class CloseAllPositionsCommand extends AbstractCommand
         }
 
         foreach ($res as $position) {
-            $this->orderService->closeByMarket($position, $position->size);
+            $this->orderService->closeByMarket($position, $positionPart->of($position->size));
         }
 
         return Command::SUCCESS;
