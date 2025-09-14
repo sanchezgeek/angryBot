131708a7258e328406e121f8ee8c6ece6e0ea7fa
Sanchez <sanchezgeek@gmail.com>
2025-09-14 10:04:18 +0300

    remove admin/assets

--- public/admin/assets/utils-Blx_SZM_.js
+++
@@ -1,1 +0,0 @@
-const t=(e=0)=>new Promise(s=>setTimeout(s,e)),r={email:e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)||"Please enter a valid email address",required:e=>!!e||"This field is required"};export{t as s,r as v};
